1e64c9d8408e5fe5075cc774a757dcaf6432f70e
ezinneanne <onwukaezinneanne@gmail.com>
2025-09-10 20:13:26 +0100

    add changes

--- portfolio/src/router/index.js
+++ portfolio/src/router/index.js
@@ -19,6 +19,11 @@ const router = createRouter({
       name: 'projects',
       component: () => import('../views/ProjectsView.vue'),
     },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
   ],
 })
 
